6ae29af011b445a1eb5834b5d7aa877106bd1628
Pavankumarios <kvpavan4481@gmail.com>
2022-12-08 15:05:49 +0530

    text added

--- GitDemoExample/ViewController.swift
+++ GitDemoExample/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        textLabel.text = "text"
         // Do any additional setup after loading the view.
     }
 
